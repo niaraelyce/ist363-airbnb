3fba36ba04d8cc8aae82b3ccb80d778a14b8f5df
Niara M <niaraelyce@Niaras-MacBook-Air.local>
2024-02-22 17:51:09 -0500

    Homework schedule json

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
+// console.log("js has been loaded!");
+
+const menuBtn = document.getElementById('menuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+const closeBtn = document.getElementById('closeBtn');
+
+// "event name", callback function
+menuBtn.addEventListener('click', () => {
+  // console.log("cllllllicked!!");
+  mobileMenu.classList.add('active');
+}); // end of menuBtn click
+
+closeBtn.addEventListener('click', () => {
+  // console.log("cllllllicked!!");
+  mobileMenu.classList.remove('active');
+}); // end of menuBtn click
+
+
+
+
 const displayCategory = (category, properties) => {
-  // console.log("Displaying category!");
-  const sectionElement = document.createElement("section");
-  sectionElement.classList.add("category");
-  const sectionTitle = document.createElement("h2");
+  //console.log({category});
+  const sectionElement = document.createElement('section');
+  sectionElement.classList.add('category');
+
+  const sectionTitle = document.createElement('h2');
   sectionTitle.textContent = category.label.plural;
 
   sectionElement.appendChild(sectionTitle);
-
-  // console.log(category.label.singular);
-  //1. filter properties
-
-  const filteredProperties = properties.filter((property) => {
+  
+  //console.log(category.label.singular);
+  // 1. filter properties
+  const filteredProperties = properties.filter(property => {
     // return true or false
     return category.label.singular === property.type;
   });
+
   filteredProperties.sort((a, b) => {
     if (a.name < b.name) {
       return -1;
@@ -24,40 +45,19 @@ const displayCategory = (category, properties) => {
     return 0;
   });
 
-  //console.log(filteredProperties);
-  filteredProperties.forEach((property) => {
-    const articleElement = document.createElement("article");
-    articleElement.classList.add("property");
+  //console.log({filteredProperties});
+  filteredProperties.forEach(property => {
+    const articleElement = document.createElement('article');
+    articleElement.classList.add('property');
 
-    const propertyTitle = document.createElement("h3");
-    propertyTitle.classList.add("property--title");
-    let propertyHTML = `
-    <h3>${property.name}</h3>
-    <p class="property--description">${property.description}</p>
-    <p class="property--price">${property.price}</p>
+    let propertyHtml = `
+      <h3 class="property--title">${property.name}</h3>
+      <p class="property--description">${property.description}</p>
+      <p class="property--price">${property.price}</p>
     `;
-    articleElement.innerHTML = propertyHTML;
+
+    articleElement.innerHTML = propertyHtml;
+
     sectionElement.appendChild(articleElement);
-  });
-  //end of forEach
 
-  // 2. loop and append properties
-
-  document.body.appendChild(sectionElement);
-};
-//end of displayCategory
-
-Promise.all([
-  // fetch 1
-  fetch("js/properties.json").then((response) => response.json()),
-  // fetch 2
-  fetch("js/categories.json").then((response) => response.json()),
-])
-  .then(([properties, categories]) => {
-    categories.forEach((category) => {
-      displayCategory(category, properties);
-    });
-  })
-  .catch((error) => {
-    console.error("There was a problem fetching the data:", error);
-  });
+  }); // end of forEach
